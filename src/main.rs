d3eba87bf9bed4287927c849e451ae1096a9e718
Weibye <13300393+Weibye@users.noreply.github.com>
2024-07-02 00:16:26 +0200

    Setup working skybox calculation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::f32::consts::PI;
 
 use bevy::{
-    pbr::{CascadeShadowConfigBuilder, ExtendedMaterial},
-    prelude::*,
+    asset::LoadState, core_pipeline::Skybox, pbr::{CascadeShadowConfigBuilder, ExtendedMaterial}, prelude::*, render::render_resource::{Extent3d, TextureViewDescriptor, TextureViewDimension}
 };
 use celestial_shaders::{AtmosphereMaterial, CelestialShadersPlugin, PlanetMaterial};
 use pcg_planet::{spherical_cuboid, PcgPlanetPlugin};
@@ -10,6 +9,7 @@ use rand::Rng;
 
 mod celestial_data;
 mod celestial_shaders;
+mod skybox;
 mod pcg_planet;
 
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
@@ -37,6 +37,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut planet_mats: ResMut<Assets<ExtendedMaterial<StandardMaterial, PlanetMaterial>>>,
     mut atmo_mats: ResMut<Assets<ExtendedMaterial<StandardMaterial, AtmosphereMaterial>>>,
+    asset_server: Res<AssetServer>,
 ) {
     let mut rng = rand::thread_rng();
     const RADIUS: f32 = 180.0;
@@ -80,15 +81,20 @@ fn setup(
     //     ..default()
     // });
 
-
+    let skybox_texture = skybox::generate_skybox(256, 256);
+    let texture_handle = asset_server.add(skybox_texture);
 
     // camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-200.0, 250.0, 500.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0.0, 0.0, 500.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
         PanOrbitCamera::default(),
+        Skybox {
+            image: texture_handle.clone(),
+            brightness: 1000.0,
+        },
     ));
 
     // ambient light
@@ -120,9 +126,45 @@ fn setup(
         .into(),
         ..default()
     });
+
+    // commands.insert_resource(Cubemap {
+    //     is_loaded: false,
+    //     image_handle: texture_handle,
+    // });
 }
 
+#[derive(Resource)]
+struct Cubemap {
+    is_loaded: bool,
+    image_handle: Handle<Image>,
+}
 
+// fn asset_loaded(
+//     asset_server: Res<AssetServer>,
+//     mut images: ResMut<Assets<Image>>,
+//     mut cubemap: ResMut<Cubemap>,
+//     mut skyboxes: Query<&mut Skybox>,
+// ) {
+//     if !cubemap.is_loaded && asset_server.load_state(&cubemap.image_handle) == LoadState::Loaded {
+        
+//         let image = images.get_mut(&cubemap.image_handle).unwrap();
+//         // NOTE: PNGs do not have any metadata that could indicate they contain a cubemap texture,
+//         // so they appear as one texture. The following code reconfigures the texture as necessary.
+//         if image.texture_descriptor.array_layer_count() == 1 {
+//             image.reinterpret_stacked_2d_as_array(image.height() / image.width());
+//             image.texture_view_descriptor = Some(TextureViewDescriptor {
+//                 dimension: Some(TextureViewDimension::Cube),
+//                 ..default()
+//             });
+//         }
+
+//         for mut skybox in &mut skyboxes {
+//             skybox.image = cubemap.image_handle.clone();
+//         }
+
+//         cubemap.is_loaded = true;
+//     }
+// }
 
 
 fn orbit_sun(time: Res<Time>, mut sun_query: Query<(&mut Transform, &DirectionalLight)>) {
